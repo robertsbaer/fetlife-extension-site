d878e50e476d591c78bb0c46aaa81522395f20a7
robertsbaer <robertsbaer@yahoo.com>
2025-02-25 19:32:42 -0500

    app.js route2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import ChromeExtensionPage from "./pages/ChromeExtensionPage";
 
 const App = () => {
   return (
-    <Router basename="/">
+    <Router>
       <Routes>
         <Route path="/" element={<ChromeExtensionPage />} />
       </Routes>
